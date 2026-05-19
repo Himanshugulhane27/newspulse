933983f878f65f927088dac0d91464f75c0c62ff
Himanshugulhane27 <himanshugulhane27@gmail.com>
2026-05-19 05:57:47 +0530

    add test dependencies and configure jest

---
+++ backend/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js'],
+  testTimeout: 30000,
+  // Suppress console noise during tests (remove to debug)
+  silent: false,
+};
